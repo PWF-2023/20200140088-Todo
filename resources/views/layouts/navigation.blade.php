2ee1784476ba46b401384fbeeb1517f7233d4d0e
savanarsa <savanarsa03@gmail.com>
2023-03-27 20:51:56 +0700

    env,mvc, route, relationship

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,15 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
+                        {{__('Todo')}}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('User.index')" :active="request()->routeIs('user.index')">
+                        {{__('User')}}
+                    </x-nav-link>
+
                 </div>
             </div>
 
@@ -69,6 +78,14 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
+                {{ __('Todo') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('User.index')" :active="request()->routeIs('user.index')">
+                {{ __('User') }}
             </x-responsive-nav-link>
         </div>
 
